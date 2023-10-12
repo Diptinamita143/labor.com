2c0b57f02978fdf797645cd213cf4f75f700498e
Diptinamita143 <deeptinanda2000@gmail.com>
2023-10-12 12:09:39 +0530

    labor

---
+++ labor_project/labor_project/Models/dal_.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labor_project.Models
+{
+    public class dal_
+    {
+      public List<my_profile> disp()
+        {
+
+        }
+    }
+}
---
+++ labor_project/labor_project/Models/gconn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace labor_project.Models
+{
+ 
+    public class gconn
+    {
+        static string con;
+        public static gconn()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"]
+                                                        .ToString());
+        }
+    }
+}
